cad0936118a96350816d00994e4368566be66cbc
Karl Taylor <karl_taylor@me.com>
2016-02-10 21:43:25 +0000

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.set('port', 1337-1);
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade');
+
+// static files (cache for 30 days in production)
+app.use('/assets', express.static(__dirname + '/assets'))
+
+// Require routes
+require('./routes')(app)
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running at http://localhost:' + app.get('port'))
+})
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp      = require('gulp'),
+  stylus      = require('gulp-stylus'),
+  browserSync = require('browser-sync'),
+  browserify  = require('browserify'),
+  babelify    = require('babelify'),
+  source      = require('vinyl-source-stream'),
+  buffer      = require('vinyl-buffer'),
+  reload      = browserSync.reload
+
+gulp.task('css', function () {
+  gulp.src('app/assets/styl/index.styl')
+    .pipe(stylus({
+      compress: true,
+      'include css': true
+    }))
+    .pipe(gulp.dest('app/assets/dist'))
+    .pipe(reload({stream:true}))
+})
+
+gulp.task('js', function () {
+  return browserify('app/assets/js/index.js', { debug: true })
+    .transform("babelify", {presets: "es2015"})
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('app/assets/dist/'))
+    .pipe(reload({stream:true}))
+})
+
+gulp.task('dev', ['css'], function() {
+  browserSync.init({
+    proxy: 'localhost:1336',
+    port: 1337,
+    open: false
+  })
+  gulp.watch("app/assets/styl/**/*.styl", ['css'])
+  gulp.watch("app/assets/js/**/*.js", ['js'])
+  gulp.watch("app/views/**/*.jade").on('change', reload)
+})
+
+gulp.task('serve', ['dev'])
