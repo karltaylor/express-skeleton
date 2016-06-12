12aab69b070fcee47a2120b851e2b33a7a0f0d15
Karl Taylor <karl_taylor@me.com>
2016-06-12 18:46:37 +0100

    removing mongoose from master

--- app/index.js
+++ app/index.js
@@ -1,5 +1,4 @@
 var express   = require('express')
-var mongoose  = require('mongoose')
 var app = express()
 
 app.set('port', process.env.PORT || 1337 - 1)
@@ -8,13 +7,6 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
 app.use('/assets', express.static(__dirname + '/assets'))
-
-mongoose.connect('mongodb://localhost/test', function(err) {
-  if (err)
-    console.log(err + "\nThe development Mongo Server is not running. Run '$ mongod' in the terminal.")
-  else
-    console.log('Connected to MongoDB')
-})
 
 require('./routes')(app)
 
