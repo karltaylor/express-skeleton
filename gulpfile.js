e127d80563550b0634bd5e2936cacf43da28797f
Karl Taylor <karl_taylor@me.com>
2017-01-10 21:01:30 +0000

    changed to standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp      = require('gulp'),
-  stylus      = require('gulp-stylus'),
-  browserSync = require('browser-sync'),
-  browserify  = require('browserify'),
-  babelify    = require('babelify'),
-  source      = require('vinyl-source-stream'),
-  buffer      = require('vinyl-buffer'),
-  autoprefixer = require('gulp-autoprefixer'),
-  reload      = browserSync.reload
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+const browserSync = require('browser-sync')
+const browserify = require('browserify')
+const babelify = require('babelify')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const autoprefixer = require('gulp-autoprefixer')
+const reload = browserSync.reload
 
 gulp.task('css', function () {
   gulp.src('app/assets/styl/index.styl')
